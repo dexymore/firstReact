65d5b85806757531a54938e11bc63f827adae61e
ahmedFATHY <af773942@gmail.com>
2022-11-13 22:10:32 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import { HashRouter } from 'react-router-dom'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -12,7 +12,10 @@ import '@fortawesome/fontawesome-free/css/all.min.css'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+       <HashRouter>
     <App />
+    </HashRouter>
+
   </React.StrictMode>
 );
 
