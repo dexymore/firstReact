5eeffbe7ee77a3cf4ff725003b0c50a9b0b8f5f6
ahmedFATHY <af773942@gmail.com>
2022-11-13 21:10:12 +0200

    trying to fixing github pages issue

--- src/App.js
+++ src/App.js
@@ -6,11 +6,11 @@ import Home from "./home/Home";
 import Portofolio from "./portfolio/Portofolio";
 import About from "./about/About";
 import Contact from "./contact/Contact";
-import Notfound from "./notfound/Notfound";
+
 import {createBrowserRouter,RouterProvider} from 'react-router-dom'
 import Layout from "./layout/Layout"
 let routes= createBrowserRouter([
-  {path: '/' ,element: <Layout></Layout>,errorElement:<Notfound></Notfound> ,children:[
+  {path: '/' ,element: <Layout></Layout> ,children:[
     {index:true,element:<Home></Home>},
 
 {path:'portofolio',element: <Portofolio></Portofolio>},
